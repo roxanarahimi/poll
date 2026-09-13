431ad271e8e9a350b75d5cb28befa924217b0123
Roxana <ms.roxanarahimi@gmail.com>
2026-09-13 09:34:35 +0330

    second

--- src/views/Poll.vue
+++ src/views/Poll.vue
@@ -2,7 +2,7 @@
   <div class="w-100">
     <div class="w-100 vh-100">
       <div class=" h-100 w-100 text-center">
-        <questions />
+        <questions/>
       </div>
     </div>
   </div>
@@ -12,28 +12,29 @@
 import Questions from "@/components/Questions.vue";
 import {onMounted, ref} from "vue";
 import App from "@/App.vue";
+
 export default {
   name: 'Poll',
-  components:{Questions,},
-  setup(){
+  components: {Questions,},
+  setup() {
     const user = ref({})
-    onMounted(()=>{
-      let user_ =JSON.parse(localStorage.getItem('user'));
-      if(user_.id){
-        axios.get(App.setup().url+'user/'+user_.id)
-            .then((res)=>{
+    onMounted(() => {
+      let user_ = JSON.parse(localStorage.getItem('user'));
+      if (user_) {
+        axios.get(App.setup().url + 'user/' + user_.id)
+            .then((res) => {
               user.value = res.data
-              localStorage.setItem('user',JSON.stringify(res.data));
+              localStorage.setItem('user', JSON.stringify(res.data));
+              if (!res.data) {
+                window.location = '/'
+              }
+              if (res.data.voted) {
+                window.location = '/'
+              }
             })
-            .catch((error)=>console.error(error))
-        console.log(user.value)
-        if(!user.value){
-          window.location = '/'
-        }
-        if(user.value.voted){
-          window.location = '/'
-        }
-      }else{
+            .catch((error) => console.error(error));
+
+      } else {
         window.location = '/'
       }
 
